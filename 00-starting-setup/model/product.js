b2835f9b20172da32c77a1d6dcc0be7ac0eb477a
igedanielolumide@gmail.com <igedanielolumide@gmail.com>
2022-11-08 14:02:37 +0100

    dynamic route

--- 00-starting-setup/model/product.js
+++ 00-starting-setup/model/product.js
@@ -23,6 +23,7 @@ module.exports = class Product {
     this.price = price
   }
   save() {
+    this.id =  Math.random()
     getProductsFromFile(products => {
         products.push(this)
         fs.writeFile(p, JSON.stringify(products), (err) => {
