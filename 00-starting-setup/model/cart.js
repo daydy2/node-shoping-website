b16cd26bf18582fdae65518b1f3ce9cf2e5cab48
igedanielolumide@gmail.com <igedanielolumide@gmail.com>
2022-11-09 15:26:10 +0100

    adding model -cart

---
+++ 00-starting-setup/model/cart.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path =  require('path')
+
+const p = path.join(
+    path.dirname(require.main.filename),
+    "data",
+    "cart.json"
+);
+
+module.exports = class Cart {
+    static addProduct(id){
+        fs.readFile(p, (err, fileContent) => {
+            let cart = {products: [], totalPrice: 0}
+            if (!err){
+                cart = JSON.parse(fileContent)
+            }
+        })
+    }
+}
